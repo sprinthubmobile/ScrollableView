231e70289574dd6893453c29330456dff485be3c
Mark Boleigha <donhoenix@gmail.com>
2020-02-15 18:47:00 +0100

    create project

---
+++ ScrollableView/ScrollableView.h
@@ -0,0 +1,19 @@
+//
+//  ScrollableView.h
+//  ScrollableView
+//
+//  Created by Sprinthub on 15/02/2020.
+//  Copyright © 2020 Sprinthub Mobile. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ScrollableView.
+FOUNDATION_EXPORT double ScrollableViewVersionNumber;
+
+//! Project version string for ScrollableView.
+FOUNDATION_EXPORT const unsigned char ScrollableViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ScrollableView/PublicHeader.h>
+
+
